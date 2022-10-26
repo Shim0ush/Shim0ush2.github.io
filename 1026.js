0c3ffffff32e5820cbb1a81b13b811574154ca4e
Shim0ush <amarecraft@gmail.com>
2022-10-26 18:59:44 +0900

    1026第5回

---
+++ 1026.js
@@ -0,0 +1,23 @@
+function hello() {
+  console.log ("こんにちは，こんにちは");
+}
+
+function helloSomeone(name) {
+  console.log (`こんにちは: ${name}さん`);
+}
+
+function helloCircleArea(radius) {
+  console.log (radius * radius * Math.PI);
+}
+
+function helloRectangeleArea(width, height) {
+  console.log (width * height);
+}
+
+function circleArea(radius) {
+  return  radius * radius * Math.PI;
+}
+
+function rectangleArea(width, height) {
+  return width * height;
+}
---
+++ modal.js
@@ -0,0 +1,10 @@
+function showModal(){
+  console.log("showModal()の実行テスト");
+  let modal = document.querySelector("#modal");
+  modal.innerHTML = "ボタンが押されました";
+}
+
+function modal2() {
+  let modalcolor = document.querySelector("#modal2");
+  modal2.element.style.backgroundColor = red;
+}
